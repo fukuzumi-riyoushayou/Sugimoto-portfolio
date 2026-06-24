a1971fe49108e3746dda931d7df23438603ffab9
fukuzumi-riyoushayou <wsc00173@wonderstorage-c.com>
2026-06-24 10:04:56 +0900

    v1.2.0
    トップへ戻るボタンの実装
    スクロールに合わせてコンテンツをふわっと表示

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,4 +14,39 @@ $(function(){
         $('#menu-content').slideUp().removeClass('menu-content-open');
         $("#menu-btn-check").prop("checked",false);
     });
+
+    $(window).on('load scroll',function() {
+        if ($(this).scrollTop() > 200) {
+            $('.gotop-btn').addClass('active');
+        } else {
+            $('.gotop-btn').removeClass('active');
+        }
+    });
+    $(window).on("scroll", function() {
+	var scrollHeight = $(document).height();
+	var scrollPosition = $(window).height() + $(window).scrollTop();
+	if ((scrollHeight - scrollPosition) <= 100) {
+	    $('.gotop-btn').removeClass('active');
+	}
+    });
+
+    // 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+//ふわっと動くきっかけのクラス名と動きのクラス名の設定
+    $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+        $(this).addClass('fadeUp');
+        // 画面内に入ったらfadeInというクラス名を追記
+        }
+    });
+}
+    // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+        fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
 });
